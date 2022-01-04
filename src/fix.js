023b83db904f59b9b435033af3c76381ed72bd99
metinozkan <metin.ozkn@hotmail.com>
2022-01-04 21:57:33 +0300

    first commit

---
+++ src/fix.js
@@ -0,0 +1,7 @@
+import * as THREECJS from 'three'
+import * as THREE from 'three/build/three.module'
+
+// This hacks around threejs broken module resolution in CJS environments
+// See: https://github.com/mrdoob/three.js/issues/17220
+// Only needed for codesandbox, because it resolves to CJS by default
+// Object.assign(THREE, THREECJS)
